d7c7a9c7938757933f6b3aa81b90a86c28c745b8
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-05-05 15:13:42 +0200

    ADD STRUCTURE

---
+++ script.js
@@ -0,0 +1,16 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+const App = vue.createApp({
+    data(){
+        return{
+            saluto : 'ciao a tutti'
+        }
+    }
+});
+
+
+App.mount('#App');
+
