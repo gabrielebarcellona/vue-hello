b8d1a61791b875d193eb9ba33341f5b99d0ab772
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-05-05 16:14:45 +0200

    last commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 
-const App = vue.createApp({
+const App = Vue.createApp({
     data(){
         return{
             saluto : 'ciao a tutti'
